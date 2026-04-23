2e44bfc25489f47f7d5704c735298627278b06c6
opilane <opilane@tpt.lan>
2026-04-23 12:55:41 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const url ="https://tinkr.tech/sdb/poly/wander"
+const url ="tinkr.tech/sdb/my_wanderworld/wanderworld";
+document.getElementById("join").addEventListener("click", async () => {
+    const username = document.getElementById("name").value.trim();
+ 
+    if (!username) return;
+ 
+    const res = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ action: "join", username })
+    });
+ 
+    const data = await res.json();
+ 
+    playerKey = data.player_key;
+    myUsername = username;
+ 
+    localStorage.setItem("playerKey", playerKey);
+    localStorage.setItem("myUsername", myUsername);
+
+let playerKey = localStorage.getItem("playerKey");
+let myUsername = localStorage.getItem("myUsername");
+});
